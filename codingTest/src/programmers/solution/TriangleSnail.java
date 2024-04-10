9b017f0e824b8bb3c82c0a18378abe3e9566948c
honinbo6355 <honinbo6355@nate.com>
2024-04-10 23:42:03 +0900

    프로그래스 - 삼각 달팽이 풀이 완료

---
+++ codingTest/src/programmers/solution/TriangleSnail.java
@@ -0,0 +1,46 @@
+package programmers.solution;
+
+import java.util.Arrays;
+
+/*
+- https://school.programmers.co.kr/learn/courses/30/lessons/68645
+- 모범 답안 https://minhamina.tistory.com/58
+- 반복문을 더 줄일 수 있다.
+*/
+
+public class TriangleSnail {
+    public static void main(String[] args) {
+        TriangleSnail t = new TriangleSnail();
+        System.out.println(Arrays.toString(t.solution(4)));
+        System.out.println(Arrays.toString(t.solution(5)));
+    }
+
+    public int[] solution(int n) {
+        int[][] arr = new int[n][n];
+        int count = n;
+        int num = 1;
+
+        for (int i=0, j=0, k=0; i<n; i+=3, j+=2, k++) {
+            int row = j-1;
+            int column = k;
+            for (int l=0; l<count; l++) {
+                arr[++row][column] = num++;
+            }
+            count--;
+            for (int l=0; l<count; l++) {
+                arr[row][++column] = num++;
+            }
+            count--;
+            for (int l=0; l<count; l++) {
+                arr[--row][--column] = num++;
+            }
+            count--;
+        }
+
+        return Arrays.stream(arr)
+                .flatMapToInt(Arrays::stream)
+                .filter(e -> e != 0)
+                .toArray();
+    }
+}
+
