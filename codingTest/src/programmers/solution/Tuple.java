1bbae7c2688ac07c1a9ddab94881ce0305c93140
honinbo6355 <honinbo6355@nate.com>
2024-05-22 03:06:51 +0900

    프로그래머스 - 튜플 두번째 풀이 완료

--- codingTest/src/programmers/solution/Tuple.java
+++ codingTest/src/programmers/solution/Tuple.java
@@ -12,18 +12,42 @@ public class Tuple {
         System.out.println(Arrays.toString(t.solution("{{2},{2,1},{2,1,3},{2,1,3,4}}")));
     }
 
+    // 풀이1
+//    public int[] solution(String s) {
+//        // 각 원소의 등장 횟수를 세기 위해 Map으로 변환
+//        Map<String, Long> frequencyMap = Arrays.stream(s.split("[{},]+"))
+//                .filter(element -> !"".equals(element))
+//                .collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+//
+//        // 등장 횟수가 높은 순서대로 정렬된 List 생성
+//        List<Map.Entry<String, Long>> sortedEntries = new ArrayList<>(frequencyMap.entrySet());
+//        sortedEntries.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+//
+//        return sortedEntries.stream()
+//                .mapToInt(entry -> Integer.parseInt(entry.getKey()))
+//                .toArray();
+//    }
+    
+    // 풀이2
     public int[] solution(String s) {
-        // 각 원소의 등장 횟수를 세기 위해 Map으로 변환
-        Map<String, Long> frequencyMap = Arrays.stream(s.split("[{},]+"))
-                .filter(element -> !"".equals(element))
-                .collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        int[] arr = Arrays.stream(s.split("[{,}]"))
+                .filter(e -> !e.equals(""))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
 
-        // 등장 횟수가 높은 순서대로 정렬된 List 생성
-        List<Map.Entry<String, Long>> sortedEntries = new ArrayList<>(frequencyMap.entrySet());
-        sortedEntries.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
 
-        return sortedEntries.stream()
-                .mapToInt(entry -> Integer.parseInt(entry.getKey()))
+        for (int element : arr) {
+            treeMap.put(element, treeMap.getOrDefault(element, 0)+1);
+        }
+
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(treeMap.entrySet());
+
+        Collections.sort(list, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+
+        return list.stream()
+                .map(entry -> entry.getKey())
+                .mapToInt(e -> e)
                 .toArray();
     }
 }
