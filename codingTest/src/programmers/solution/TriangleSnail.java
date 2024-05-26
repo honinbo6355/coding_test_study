6b428af12e52471748d55dc3197e01216512431e
honinbo6355 <honinbo6355@nate.com>
2024-05-27 02:20:29 +0900

    프로그래머스 - 삼각 달팽이 두번째 풀이 완료

--- codingTest/src/programmers/solution/TriangleSnail.java
+++ codingTest/src/programmers/solution/TriangleSnail.java
@@ -15,30 +15,57 @@ public class TriangleSnail {
         System.out.println(Arrays.toString(t.solution(5)));
     }
 
+//    public int[] solution(int n) {
+//        int[][] arr = new int[n][n];
+//        int count = n;
+//        int num = 1;
+//
+//        for (int i=0, j=0, k=0; i<n; i+=3, j+=2, k++) {
+//            int row = j-1;
+//            int column = k;
+//            for (int l=0; l<count; l++) {
+//                arr[++row][column] = num++;
+//            }
+//            count--;
+//            for (int l=0; l<count; l++) {
+//                arr[row][++column] = num++;
+//            }
+//            count--;
+//            for (int l=0; l<count; l++) {
+//                arr[--row][--column] = num++;
+//            }
+//            count--;
+//        }
+//
+//        return Arrays.stream(arr)
+//                .flatMapToInt(Arrays::stream)
+//                .filter(e -> e != 0)
+//                .toArray();
+//    }
+
     public int[] solution(int n) {
         int[][] arr = new int[n][n];
-        int count = n;
-        int num = 1;
+        int num = 1, count = 0, row = -1, column = 0;
 
-        for (int i=0, j=0, k=0; i<n; i+=3, j+=2, k++) {
-            int row = j-1;
-            int column = k;
-            for (int l=0; l<count; l++) {
+        while (count < n) {
+            for (int i=count; i<n; i++) {
                 arr[++row][column] = num++;
             }
-            count--;
-            for (int l=0; l<count; l++) {
+            count++;
+
+            for (int j=count; j<n; j++) {
                 arr[row][++column] = num++;
             }
-            count--;
-            for (int l=0; l<count; l++) {
+            count++;
+
+            for (int k=count; k<n; k++) {
                 arr[--row][--column] = num++;
             }
-            count--;
+            count++;
         }
 
         return Arrays.stream(arr)
-                .flatMapToInt(Arrays::stream)
+                .flatMapToInt(a -> Arrays.stream(a))
                 .filter(e -> e != 0)
                 .toArray();
     }
