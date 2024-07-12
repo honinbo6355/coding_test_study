31d10dec251df0ee7ee8837f7021592a49d2d270
honinbo6355 <honinbo6355@nate.com>
2024-07-12 15:23:29 +0900

    leetcode - N-ary Tree Preorder Traversal, N-ary Tree Postorder Traversal 두번째 풀이 완료

--- codingTest/src/leetcode/solution/TreePostorderTraversal.java
+++ codingTest/src/leetcode/solution/TreePostorderTraversal.java
@@ -3,28 +3,51 @@ package leetcode.solution;
 import java.util.ArrayList;
 import java.util.List;
 
+// - https://leetcode.com/problems/n-ary-tree-postorder-traversal/description/
+// 첫번째 풀이
+//public class TreePostorderTraversal {
+//    public List<Integer> answer = new ArrayList<>();
+//
+//    public List<Integer> postorder(Node root) {
+//        if (root == null) {
+//            return answer;
+//        }
+//
+//        traversal(root);
+//        answer.add(root.val);
+//
+//        return answer;
+//    }
+//
+//    public void traversal(Node root) {
+//        if (root == null) {
+//            return;
+//        }
+//
+//        for (Node node : root.children) {
+//            traversal(node);
+//            answer.add(node.val);
+//        }
+//    }
+//}
+
+// 두번째 풀이
 public class TreePostorderTraversal {
-    public List<Integer> answer = new ArrayList<>();
-
     public List<Integer> postorder(Node root) {
-        if (root == null) {
-            return answer;
-        }
-
-        traversal(root);
-        answer.add(root.val);
-
-        return answer;
+        List<Integer> list = new ArrayList<>();
+        dfs(root, list);
+        return list;
     }
 
-    public void traversal(Node root) {
-        if (root == null) {
+    private void dfs(Node node, List<Integer> list) {
+        if (node == null) {
             return;
         }
 
-        for (Node node : root.children) {
-            traversal(node);
-            answer.add(node.val);
+        for (Node n : node.children) {
+            dfs(n, list);
         }
+
+        list.add(node.val);
     }
 }
--- codingTest/src/leetcode/solution/TreePreorderTraversal.java
+++ codingTest/src/leetcode/solution/TreePreorderTraversal.java
@@ -20,20 +20,43 @@ class Node {
     }
 };
 
+// - https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
+// 첫번째 풀이
+//public class TreePreorderTraversal {
+//    public List<Integer> answer = new ArrayList<>();
+//
+//    public List<Integer> preorder(Node root) {
+//        if (root == null) {
+//            return answer;
+//        }
+//
+//        answer.add(root.val);
+//
+//        for (Node node : root.children) {
+//            preorder(node);
+//        }
+//
+//        return answer;
+//    }
+//}
+
+// 두번째 풀이
 public class TreePreorderTraversal {
-    public List<Integer> answer = new ArrayList<>();
-
     public List<Integer> preorder(Node root) {
-        if (root == null) {
-            return answer;
-        }
-
-        answer.add(root.val);
-
-        for (Node node : root.children) {
-            preorder(node);
-        }
-
+        List<Integer> answer = new ArrayList<>();
+        dfs(root, answer);
         return answer;
+    }
+
+    private void dfs(Node node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+
+        list.add(node.val);
+
+        for (Node n : node.children) {
+            dfs(n, list);
+        }
     }
 }
