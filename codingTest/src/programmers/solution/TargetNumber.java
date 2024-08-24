d7fff8b5277a7df956c854ea37d7d73c1e7734e3
honinbo6355 <honinbo6355@nate.com>
2024-08-24 18:26:22 +0900

    프로그래머스 - 타겟 넘버 두번째 풀이 완료

--- codingTest/src/programmers/solution/TargetNumber.java
+++ codingTest/src/programmers/solution/TargetNumber.java
@@ -6,30 +6,56 @@ import java.util.Arrays;
  * - https://school.programmers.co.kr/learn/courses/30/lessons/43165
  */
 
-public class TargetNumber {
-    private static int answer = 0;
+// 첫번째 풀이
+//public class TargetNumber {
+//    private static int answer = 0;
+//
+//    public static void main(String[] args) {
+//        TargetNumber t = new TargetNumber();
+//        System.out.println(t.solution(new int[] {1,1,1,1,1}, 3));
+////        System.out.println(t.solution(new int[] {4, 1, 2, 1}, 4));
+//    }
+//
+//    public int solution(int[] numbers, int target) {
+//        dfs(numbers, target, 0);
+//        return answer;
+//    }
+//
+//    private void dfs(int[] numbers, int target, int start) {
+//        if (numbers.length == start) {
+//            if (Arrays.stream(numbers).sum() == target) {
+//                answer++;
+//            }
+//            return;
+//        }
+//
+//        dfs(numbers, target, start+1);
+//        numbers[start] *= -1;
+//        dfs(numbers, target, start+1);
+//    }
+//}
 
+// 두번째 풀이
+public class TargetNumber {
     public static void main(String[] args) {
         TargetNumber t = new TargetNumber();
         System.out.println(t.solution(new int[] {1,1,1,1,1}, 3));
-//        System.out.println(t.solution(new int[] {4, 1, 2, 1}, 4));
+        System.out.println(t.solution(new int[] {4,1,2,1}, 4));
     }
 
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0);
-        return answer;
+        int[] answer = new int[] {0};
+        dfs(numbers, 0, 0, target, answer);
+        return answer[0];
     }
-
-    private void dfs(int[] numbers, int target, int start) {
-        if (numbers.length == start) {
-            if (Arrays.stream(numbers).sum() == target) {
-                answer++;
+    private void dfs(int[] numbers, int idx, int sum, int target, int[] answer) {
+        if (numbers.length <= idx) {
+            if (sum == target) {
+                answer[0]++;
             }
             return;
         }
-
-        dfs(numbers, target, start+1);
-        numbers[start] *= -1;
-        dfs(numbers, target, start+1);
+        dfs(numbers, idx+1, sum+numbers[idx], target, answer);
+        dfs(numbers, idx+1, sum-numbers[idx], target, answer);
     }
 }
