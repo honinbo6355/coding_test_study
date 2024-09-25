975969bd0a65d3db139acb6dc91a70f226bc963e
honinbo6355 <honinbo6355@nate.com>
2024-09-25 21:35:44 +0900

    leetcode - FindRightInterval 두번째 문제 풀이 완료

--- codingTest/src/leetcode/solution/FindRightInterval.java
+++ codingTest/src/leetcode/solution/FindRightInterval.java
@@ -15,38 +15,81 @@ public class FindRightInterval {
         System.out.println(Arrays.toString(f.findRightInterval(new int[][] {{4,5},{2,3},{1,2}})));
     }
 
+    // 첫번째 풀이
+//    public int[] findRightInterval(int[][] intervals) {
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < intervals.length; i++) {
+//            map.put(intervals[i][0], i);
+//        }
+//        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
+//        entryList.sort(Map.Entry.comparingByKey());
+//
+//        int[] answer = new int[intervals.length];
+//
+//        for (int i=0; i<intervals.length; i++) {
+//            int targetVal = intervals[i][1];
+//            int idx = binarySearch(entryList, targetVal);
+//            answer[i] = idx;
+//        }
+//        return answer;
+//    }
+//    private int binarySearch(List<Map.Entry<Integer, Integer>> entryList, int targetVal) {
+//        int start=0;
+//        int end=entryList.size()-1;
+//        int result=-1;
+//
+//        while (start<=end) {
+//            int mid = (start+end)/2;
+//            int compareVal = entryList.get(mid).getKey();
+//            if (compareVal >= targetVal) {
+//                result = entryList.get(mid).getValue(); // 결과 저장
+//                end = mid-1; // 더 작은 값을 찾기 위해 end 줄이기
+//            } else {
+//                start = mid+1;
+//            }
+//        }
+//        return result;
+//    }
+
+    // 두번째 풀이
     public int[] findRightInterval(int[][] intervals) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < intervals.length; i++) {
+        int[] arr = new int[intervals.length];
+
+        for (int i=0; i<intervals.length; i++) {
             map.put(intervals[i][0], i);
+            arr[i] = intervals[i][0];
         }
-        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
-        entryList.sort(Map.Entry.comparingByKey());
+
+        Arrays.sort(arr);
 
         int[] answer = new int[intervals.length];
 
         for (int i=0; i<intervals.length; i++) {
-            int targetVal = intervals[i][1];
-            int idx = binarySearch(entryList, targetVal);
+            int idx = binarySearch(arr, map, intervals, i);
             answer[i] = idx;
         }
+
         return answer;
     }
-    private int binarySearch(List<Map.Entry<Integer, Integer>> entryList, int targetVal) {
-        int start=0;
-        int end=entryList.size()-1;
-        int result=-1;
 
-        while (start<=end) {
-            int mid = (start+end)/2;
-            int compareVal = entryList.get(mid).getKey();
-            if (compareVal >= targetVal) {
-                result = entryList.get(mid).getValue(); // 결과 저장
-                end = mid-1; // 더 작은 값을 찾기 위해 end 줄이기
+    private int binarySearch(int[] arr, Map<Integer, Integer> map, int[][] intervals, int currentIdx) {
+        int[] targetInterval = intervals[currentIdx];
+        int idx = -1;
+        int startIdx = 0;
+        int endIdx = intervals.length-1;
+
+        while (startIdx <= endIdx) {
+            int midIdx = (startIdx+endIdx)/2;
+
+            if (arr[midIdx] >= targetInterval[1]) {
+                idx = map.get(arr[midIdx]);
+                endIdx = midIdx-1;
             } else {
-                start = mid+1;
+                startIdx = midIdx+1;
             }
         }
-        return result;
+
+        return idx;
     }
 }
