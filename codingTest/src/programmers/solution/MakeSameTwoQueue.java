ed135682f97db61c4568c7762521683b78db067c
honinbo6355 <honinbo6355@nate.com>
2024-10-10 17:56:02 +0900

    프로그래머스 - 두 큐 합 같게 만들기 문제 두번째 풀이 완료

--- codingTest/src/programmers/solution/MakeSameTwoQueue.java
+++ codingTest/src/programmers/solution/MakeSameTwoQueue.java
@@ -17,44 +17,84 @@ public class MakeSameTwoQueue {
         System.out.println(m.solution(new int[] {1,1}, new int[] {1,5}));
     }
 
+    // 첫번째 풀이
+//    public int solution(int[] queue1, int[] queue2) {
+//        int count = 0;
+//        long q1Sum = 0, q2Sum = 0;
+//
+//        Queue<Integer> q1 = new LinkedList<>();
+//        Queue<Integer> q2 = new LinkedList<>();
+//
+//        for (int element : queue1) {
+//            q1Sum += element;
+//            q1.offer(element);
+//        }
+//
+//        for (int element : queue2) {
+//            q2Sum += element;
+//            q2.offer(element);
+//        }
+//
+//        while (count < queue1.length*4) {
+//            if (q1Sum == q2Sum) {
+//                break;
+//            } else if (q1Sum > q2Sum) {
+//                int q1Element = q1.poll();
+//                q2.offer(q1Element);
+//                q1Sum -= q1Element;
+//                q2Sum += q1Element;
+//            } else {
+//                int q2Element = q2.poll();
+//                q1.offer(q2Element);
+//                q1Sum += q2Element;
+//                q2Sum -= q2Element;
+//            }
+//            count++;
+//        }
+//
+//        if (q1Sum != q2Sum) {
+//            return -1;
+//        }
+//
+//        return count;
+//    }
+
+    // 두번째 풀이
     public int solution(int[] queue1, int[] queue2) {
-        int count = 0;
-        long q1Sum = 0, q2Sum = 0;
+        long queueSum1 = 0, queueSum2 = 0;
+        Queue<Integer> q1 = new LinkedList<>(), q2 = new LinkedList<>();
+        int answer = 0;
 
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
+        for (int i=0; i<queue1.length; i++) {
+            queueSum1 += queue1[i];
+            queueSum2 += queue2[i];
 
-        for (int element : queue1) {
-            q1Sum += element;
-            q1.offer(element);
+            q1.add(queue1[i]);
+            q2.add(queue2[i]);
         }
 
-        for (int element : queue2) {
-            q2Sum += element;
-            q2.offer(element);
+        if (queueSum1 == queueSum2) {
+            return answer;
         }
 
-        while (count < queue1.length*4) {
-            if (q1Sum == q2Sum) {
-                break;
-            } else if (q1Sum > q2Sum) {
-                int q1Element = q1.poll();
-                q2.offer(q1Element);
-                q1Sum -= q1Element;
-                q2Sum += q1Element;
+        for (int i=0; i<queue1.length*4; i++) {
+            if (queueSum1 == queueSum2) {
+                return answer;
+            } else if (queueSum1 < queueSum2) {
+                int qElement2 = q2.poll();
+                q1.add(qElement2);
+                queueSum1 += qElement2;
+                queueSum2 -= qElement2;
             } else {
-                int q2Element = q2.poll();
-                q1.offer(q2Element);
-                q1Sum += q2Element;
-                q2Sum -= q2Element;
+                int qElement1 = q1.poll();
+                q2.add(qElement1);
+                queueSum1 -= qElement1;
+                queueSum2 += qElement1;
             }
-            count++;
+
+            answer++;
         }
 
-        if (q1Sum != q2Sum) {
-            return -1;
-        }
-
-        return count;
+        return -1;
     }
 }
