152eda67071dffac69ffe3796da00e88212197b0
송세일 <songseil@songseil-ui-MacBookPro.local>
2024-10-21 01:07:41 +0900

    프로그래머스 - 단속카메라 문제 두번째 풀이 완료

--- codingTest/src/programmers/solution/SpeedCamera.java
+++ codingTest/src/programmers/solution/SpeedCamera.java
@@ -14,10 +14,32 @@ public class SpeedCamera {
         System.out.println(s.solution(new int[][] {{-20,-15}, {-14,-5}, {-18,-13}, {-5,-3}}));
     }
 
-    public int solution(int[][] routes) {
-        int answer = 0;
+    // 첫번째 풀이
+//    public int solution(int[][] routes) {
+//        int answer = 0;
+//
+//        // 진출 지점을 기준으로 오름차순 정렬 하는게 핵심이다.
+//        Arrays.sort(routes, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                return Integer.compare(o1[1], o2[1]);
+//            }
+//        });
+//
+//        int cam = Integer.MIN_VALUE;
+//        for (int[] route : routes) {
+//            if (cam < route[0]) { // 설치한 카메라 위치보다 진출 지점이 더 큰 경로(겹치지 않는 곳)는 새로운 카메라를 설치해줘야 한다.
+//                cam = route[1];
+//                answer++;
+//            }
+//        }
+//        return answer;
+//    }
 
-        // 진출 지점을 기준으로 오름차순 정렬 하는게 핵심이다.
+    // 두번째 풀이
+    public int solution(int[][] routes) {
+        int answer = 1;
+
         Arrays.sort(routes, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -25,13 +47,15 @@ public class SpeedCamera {
             }
         });
 
-        int cam = Integer.MIN_VALUE;
-        for (int[] route : routes) {
-            if (cam < route[0]) { // 설치한 카메라 위치보다 진출 지점이 더 큰 경로(겹치지 않는 곳)는 새로운 카메라를 설치해줘야 한다.
-                cam = route[1];
+        int position = routes[0][1];
+
+        for (int i=1; i<routes.length; i++) {
+            if (position < routes[i][0]) {
+                position = routes[i][1];
                 answer++;
             }
         }
+
         return answer;
     }
 }
